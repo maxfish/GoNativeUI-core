d29ba18107a603f205dc6ad466f393f5a3c1e3ed
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-11 22:57:52 +0200

    More debug methods

--- debug.go
+++ debug.go
@@ -2,9 +2,21 @@ package gui
 
 import (
 	"fmt"
+	"github.com/maxfish/GoNativeUI-Core/utils"
 	"reflect"
 	"strings"
 )
+
+func DimensionToString(dimension utils.Dimension) string {
+	switch dimension.Unit {
+	case utils.PixelUnit:
+		return fmt.Sprintf("%dpx", dimension.Value)
+	case utils.PercentageUnit:
+		return fmt.Sprintf("%d%%", dimension.Value)
+	}
+
+	return "-"
+}
 
 func WidgetToDebugString(str interface{}) string {
 	widget := str.(IWidget)
@@ -43,7 +55,15 @@ func WidgetToDebugString(str interface{}) string {
 		extra = "text:\"" + w.text + "\""
 	}
 
-	return fmt.Sprintf("(%s)%s %s %s %s", wType, wId, widget.Bounds().ToString(), flags, extra)
+	// Bounds
+	boundsString := fmt.Sprintf(
+		"%s[%s,%s]",
+		widget.Bounds().ToString(),
+		DimensionToString(widget.DimensionH()),
+		DimensionToString(widget.DimensionV()),
+	)
+
+	return fmt.Sprintf("(%s)%s %s %s %s", wType, wId,  boundsString, flags, extra)
 }
 
 func ContainerToTreeDebugString(c IContainer) string {
@@ -59,6 +79,20 @@ func containerToTreeDebugString(node IWidget, depth string) string {
 			s += fmt.Sprintf("%s `--", depth)
 			c := " "
 			if index < container.ChildrenCount()-1 {
+				c = "|"
+			}
+			depth += " " + c + "  "
+			s += containerToTreeDebugString(child, depth)
+			depth = depth[:len(depth)-4]
+		}
+	}
+	bc, ok := node.(*BoxContainer)
+	if ok {
+		for index, _ := range bc.Children() {
+			child := bc.children[index]
+			s += fmt.Sprintf("%s `--", depth)
+			c := " "
+			if index < bc.ChildrenCount()-1 {
 				c = "|"
 			}
 			depth += " " + c + "  "
