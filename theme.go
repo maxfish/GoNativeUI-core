65e2e28ddb547c9b007d76556b39b258e24710d6
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-15 13:31:41 +0200

    Changes to the default theme

--- theme.go
+++ theme.go
@@ -29,20 +29,18 @@ func NewDefaultTheme() *Theme {
 	t := &Theme{}
 
 	t.ScreenBackgroundColor = utils.ColorGrayi(56, 255)
-
-	//defaultFont := NewFontFromFile("assets/Roboto-Regular.fnt")
 	defaultFont := NewFontFromData(assets.FontRobotoRegularDefinition, []string{assets.FontRobotoRegularImage})
 
 	t.LabelFont = defaultFont
 	t.LabelTextColor = utils.ColorGrayi(225, 255)
 	t.LabelFontSize = 15
-	t.LabelPadding = utils.HomogeneousInsets(0)
+	t.LabelPadding = utils.Insets{Top: 10, Right: 10, Bottom: 5, Left: 5}
 
 	t.ButtonFont = defaultFont
 	t.ButtonFillColor = utils.ColorGrayi(105, 255)
 	t.ButtonTextColor = utils.ColorGrayi(232, 255)
 	t.ButtonFontSize = 15
 	t.ButtonPadding = utils.Insets{Top: 5, Right: 10, Bottom: 5, Left: 10}
-	t.ButtonAlignment = utils.Alignment{utils.AlignmentHCenter, utils.AlignmentVCenter}
+	t.ButtonAlignment = utils.Alignment{Horizontal: utils.AlignmentHCenter, Vertical: utils.AlignmentVCenter}
 	return t
 }
