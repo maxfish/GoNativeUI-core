06cefecd552c09dc323de6007d7dcc665e00f835
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-15 12:42:18 +0200

    Dimension is not necessary anymore

--- utils/alignment.go
+++ utils/alignment.go
@@ -1,25 +1,5 @@
 package utils
 
-type MeasureUnit int
-
-const (
-	NoUnit MeasureUnit = iota
-	PixelUnit
-	PercentageUnit
-)
-
-type Dimension struct {
-	Value int
-	Unit  MeasureUnit
-}
-
-func Pixels(value int) Dimension {
-	return Dimension{value, PixelUnit}
-}
-func Percentage(value int) Dimension {
-	return Dimension{value, PercentageUnit}
-}
-
 type AlignmentH uint32
 
 const (
