9c04ae44e0565cb950bf32a67c3beaeecf00f0f7
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-17 10:46:09 +0200

    buttonFillColor was missing

--- button.go
+++ button.go
@@ -18,6 +18,7 @@ func (b *Button) SetTheme(theme *Theme) {
 	b.theme = theme
 	b.font = theme.ButtonFont
 	b.textColor = theme.ButtonTextColor
+	b.fillColor = theme.ButtonFillColor
 	b.fontSize = theme.ButtonFontSize
 	b.padding = theme.ButtonPadding
 	b.contentAlignment = theme.ButtonAlignment
