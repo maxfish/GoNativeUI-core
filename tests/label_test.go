15db928782593a8ab8f760551f151825fff28f7a
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-20 20:56:05 +0200

    Adjusted label test

--- tests/label_test.go
+++ tests/label_test.go
@@ -10,24 +10,23 @@ func TestLabel(t *testing.T) {
 	g := InitDummyGui(screenWidth, screenHeight, nil)
 	defer FreeGui(g)
 	label := gui.NewLabel(textStrings[0])
-	g.Screen().AddChild(label)
 
 	// Check initial state
 	assertBoolEqual(t, label.Visible(), true)
 	assertBoolEqual(t, label.Enabled(), true)
-
-	// Measured size == Text size + Padding
-	assertIntEqual(t, label.MeasuredWidth(), textSizes[0].W()+label.Style().Padding.Left+label.Style().Padding.Right)
-	assertIntEqual(t, label.MeasuredHeight(), textSizes[0].H()+label.Style().Padding.Top+label.Style().Padding.Bottom)
-
 	assertStructEqual(t, label.Style().TextColor, g.Theme().TextColor)
 	assertStructEqual(t, label.Style().Font, g.Theme().TextFont)
 	assertStructEqual(t, label.Style().FontSize, g.Theme().TextFontSize)
-
 	assertStringEqual(t, label.Text(), textStrings[0])
 
+	// Change the text
 	label.SetText(textStrings[2])
 	assertStringEqual(t, label.Text(), textStrings[2])
+
+	// Measured size == Text size + Padding
+	label.Measure()
+	assertIntEqual(t, label.MeasuredWidth(), textSizes[2].W()+label.Style().Padding.Left+label.Style().Padding.Right)
+	assertIntEqual(t, label.MeasuredHeight(), textSizes[2].H()+label.Style().Padding.Top+label.Style().Padding.Bottom)
 
 	for i := 0; i < len(textStrings); i++ {
 		label.SetText(textStrings[i])
