a488d889182c389c9c22570e499dab096deb6e13
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-23 10:57:06 +0200

    ImageView

--- imageview.go
+++ imageview.go
@@ -1,19 +1,32 @@
 package gui
 
 import (
+	"github.com/maxfish/GoNativeUI-Core/utils"
 	"image"
 )
 
 type ImageView struct {
 	Widget
-	image image.Image
+	image        image.Image
+	contentMode  utils.FitMode
+	contentScale float32
 }
 
 func NewImageView(image image.Image) *ImageView {
 	s := &ImageView{}
 	widgetInit(s)
+	s.contentMode = utils.FitModeAlign
+	s.contentScale = 1
 	s.image = image
 	return s
+}
+
+func (i *ImageView) initStyle() {
+	//t := CurrentGui().Theme()
+	i.style = &WidgetStyle{
+		BackgroundColor:  utils.TransparentColor,
+		ContentAlignment: utils.Alignment{Horizontal: utils.AlignmentHCenter, Vertical: utils.AlignmentVCenter},
+	}
 }
 
 func (i *ImageView) Measure() {
@@ -25,9 +38,11 @@ func (i *ImageView) Measure() {
 	i.measuredFlex = i.flex
 }
 
-func (i *ImageView) Image() image.Image {
-	return i.image
-}
+func (i *ImageView) Image() image.Image            { return i.image }
+func (i *ImageView) FitMode() utils.FitMode        { return i.contentMode }
+func (i *ImageView) SetFitMode(c utils.FitMode)    { i.contentMode = c }
+func (i *ImageView) ContentScale() float32         { return i.contentScale }
+func (i *ImageView) SetContentScale(scale float32) { i.contentScale = scale }
 
 func (i *ImageView) SetImage(image image.Image) {
 	i.image = image
