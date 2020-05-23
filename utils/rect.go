69fc175ac1c3d9d52c05d7ab1208a9ba6c39478a
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-05-23 10:58:25 +0200

    Improvements on Rect

--- utils/rect.go
+++ utils/rect.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"math"
 )
 
 type Rect struct {
@@ -92,122 +91,41 @@ func (r Rect) UnionWith(other Rect) Rect {
 }
 
 func (r Rect) ContainsPoint(pointX, pointY int) bool {
-	return containsPoint(r.X, r.Y, r.W, r.H, pointX, pointY)
+	pointX -= r.X
+	pointY -= r.Y
+	return pointX >= 0 && pointY >= 0 && pointX < r.W && pointY < r.H
 }
 
-func (r Rect) ContainsRect(other Rect) bool {
-	return containsRect(r.X, r.Y, r.W, r.H, other.X, other.Y, other.W, other.H)
-}
+func (r Rect) OverlapsWith(r2 Rect) bool {
 
-func (r Rect) IntersectsRect(other Rect) bool {
-	return intersectsRect(r.X, r.Y, r.W, r.H, other.X, other.Y, other.W, other.H)
-}
+	if r.X >= r2.X+r2.W || r2.X >= r.X+r.W {
+		return false
+	}
+	if r.Y >= r2.Y+r2.H || r2.Y >= r.Y+r.H {
+		return false
+	}
 
-func containsPoint(x, y, w, h, pointX, pointY int) bool {
-	if w <= 0 || h <= 0 {
-		return false
-	}
-	if pointX < x || pointY < y {
-		return false
-	}
-	w += x
-	h += y
-	//    overflow || intersect
-	return (w < x || w >= pointX) && (h < y || h >= pointY)
-}
-
-func containsRect(x, y, w, h, rx, ry, rw, rh int) bool {
-	if (rw | rh | w | h) <= 0 {
-		return false
-	}
-	// Note: if any dimension is zero, tests below must return false...
-	if rx < x || ry < y {
-		return false
-	}
-	w += x
-	rw += rx
-	if rw <= rx {
-		// rx+rw overflowed or rw was zero, return false if...
-		// either original w or rw was zero or
-		// x+w did not overflow or
-		// the overflowed x+w is smaller than the overflowed rx+rw
-		if w >= x || rw > w {
-			return false
-		}
-	} else {
-		// rx+rw did not overflow and rw was not zero, return false if...
-		// original w was zero or
-		// x+w did not overflow and x+w is smaller than rx+rw
-		if w >= x && rw > w {
-			return false
-		}
-	}
-	h += y
-	rh += ry
-	if rh <= ry {
-		if h >= y || rh > h {
-			return false
-		}
-	} else {
-		if h >= y && rh > h {
-			return false
-		}
-	}
 	return true
 }
 
-func intersectsRect(x, y, w, h, rx, ry, rw, rh int) bool {
-	if (rw | rh | w | h) <= 0 {
-		return false
-	}
-	rw += rx
-	rh += ry
-	w += x
-	h += y
-	//      overflow || intersect
-	return (rw < rx || rw > x) &&
-		(rh < ry || rh > y) &&
-		(w < x || w > rx) &&
-		(h < y || h > ry)
-}
+func (r Rect) IntersectionWith(s Rect) Rect {
+	x2 := MinI(r.Right(), s.Right())
+	y2 := MinI(r.Bottom(), s.Bottom())
 
-func (r Rect) IntersectionWith(other Rect) Rect {
-	tx1 := r.X
-	ty1 := r.Y
-	rx1 := other.X
-	ry1 := other.Y
-	tx2 := tx1
-	tx2 += r.W
-	ty2 := ty1
-	ty2 += r.H
-	rx2 := rx1
-	rx2 += other.W
-	ry2 := ry1
-	ry2 += other.H
-	if tx1 < rx1 {
-		tx1 = rx1
+	if r.X < s.X {
+		r.X = s.X
 	}
-	if ty1 < ry1 {
-		ty1 = ry1
+	if r.Y < s.Y {
+		r.Y = s.Y
 	}
-	if tx2 > rx2 {
-		tx2 = rx2
+
+	r.W = x2 - r.X
+	r.H = y2 - r.Y
+
+	if r.W > 0 && r.H > 0 {
+		return r
 	}
-	if ty2 > ry2 {
-		ty2 = ry2
-	}
-	tx2 -= tx1
-	ty2 -= ty1
-	// tx2,ty2 will never overflow (they will never be
-	// larger than the smallest of the two source w,h)
-	// they might underflow, though...
-	if tx2 < math.MinInt32 {
-		tx2 = math.MinInt32
-	}
-	if ty2 < math.MinInt32 {
-		ty2 = math.MinInt32
-	}
-	return Rect{tx1, ty1, tx2, ty2}
+	return Rect{}
 }
 
 func (r Rect) EqualsTo(other Rect) bool {
