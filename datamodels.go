3d98b71f283ac6a568a86677511f91ef8196c91d
Massimiliano Pesce <maxfish@users.noreply.github.com>
2020-06-07 12:45:15 +0200

    Improved ListView content dimensions measurement

--- datamodels.go
+++ datamodels.go
@@ -2,6 +2,7 @@ package gui
 
 type ListModel interface {
 	ItemAt(view *ListView, row int) string
+	ItemsMaxWidth(view *ListView) int
 	ItemHeight(view *ListView) int
 	NumItems(view *ListView) int
 }
@@ -16,6 +17,18 @@ func NewStringListModel(items []string) *StringListModel {
 
 func (d *StringListModel) ItemAt(view *ListView, row int) string {
 	return d.items[row]
+}
+
+func (d *StringListModel) ItemsMaxWidth(view *ListView) int {
+	// This is slow but accurate
+	maxWidth := 0
+	for _, s := range d.items {
+		size := view.style.Font.TextSize(view.style.FontSize, s)
+		if size.W() > maxWidth {
+			maxWidth = size.W()
+		}
+	}
+	return maxWidth
 }
 
 func (d *StringListModel) ItemHeight(view *ListView) int {
--- listview.go
+++ listview.go
@@ -7,7 +7,7 @@ import (
 const (
 	DefaultRowWidth    = 150
 	DefaultRowHeight   = 20
-	DefaultVisibleRows = 20
+	DefaultVisibleRows = 10
 )
 
 type ListView struct {
@@ -30,28 +30,27 @@ func NewListView(dataModel ...ListModel) *ListView {
 	return l
 }
 
-func (l *ListView) Measure() {
-	l.measuredWidth = DefaultRowWidth
-	l.measuredFlex = l.flex
-
-	if l.dataModel != nil {
-		l.measuredHeight = l.visibleRows * l.dataModel.ItemHeight(l)
-	} else {
-		l.measuredHeight = l.visibleRows * DefaultRowHeight
-	}
-}
-
 func (l *ListView) Offset() int                      { return int(l.offset) }
 func (l *ListView) DataModel() ListModel             { return l.dataModel }
 func (l *ListView) SetDataModel(dataModel ListModel) { l.dataModel = dataModel }
 func (l *ListView) SelectedIndex() int               { return l.selectedIndex }
 func (l *ListView) SetSelectedIndex(index int)       { l.selectedIndex = index }
 
-func (l *ListView) totalHeight() int {
-	if l.dataModel == nil {
-		return 0
+func (l *ListView) Measure() {
+	l.computeContentSize()
+	l.measuredWidth = l.contentWidth + l.style.Padding.Left + l.style.Padding.Right
+	l.measuredHeight = utils.MinI(l.visibleRows * l.dataModel.ItemHeight(l), l.contentHeight) + l.style.Padding.Top + l.style.Padding.Bottom
+	l.measuredFlex = l.flex
+}
+
+func (l *ListView) computeContentSize() {
+	if l.dataModel != nil {
+		l.contentWidth = l.dataModel.ItemsMaxWidth(l)
+		l.contentHeight = l.dataModel.NumItems(l) * l.dataModel.ItemHeight(l)
+	} else {
+		l.contentWidth = DefaultRowWidth
+		l.contentHeight = l.visibleRows * DefaultRowHeight
 	}
-	return l.dataModel.NumItems(l) * l.dataModel.ItemHeight(l)
 }
 
 func (l *ListView) OnMouseCursorMoved(x, y float32) bool {
@@ -74,6 +73,6 @@ func (l *ListView) OnMouseButtonEvent(x float32, y float32, button ButtonIndex, 
 }
 
 func (l *ListView) OnMouseScrolled(x float32, y float32, scrollX, scrollY float32) bool {
-	l.offset = utils.Clamp(l.offset-scrollY, 0, float32(l.totalHeight()-l.InnerBounds().H))
+	l.offset = utils.Clamp(l.offset-scrollY, 0, float32(l.contentHeight-l.InnerBounds().H))
 	return true
 }
